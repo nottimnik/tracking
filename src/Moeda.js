bd8d0d8a4c39ac027a3d636f5ed25491d4a57f96
timnik <72356939+nottimnik@users.noreply.github.com>
2021-12-31 01:57:19 -0800

    Initial commit

---
+++ src/Moeda.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Moeda.css";
+import styled from "styled-components";
+
+const Moeda = ({
+  name,
+  place,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange
+}) => {
+  return (
+    <div className="moeda-container">
+      <div className="moeda-row">
+        <CoinPlace>{place} </CoinPlace>
+        <div className="moeda">
+          <img src={image} alt="crypto" />
+          <h2>{name}</h2>
+          <p className="moeda-symbol">{symbol}</p>
+        </div>
+        <div className="moeda-data">
+          <p className="moeda-price">${price}</p>
+          <p className="moeda-volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="moeda-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="moeda-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="moeda-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Moeda;
+
+const CoinPlace = styled.p`
+  margin-right: 10px;
+`;
